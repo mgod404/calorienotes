5884496c3caefbde35b1ec806eef9b49d95258a6
Martyn <mgodlewski1998@gmail.com>
2022-05-17 21:47:33 +0200

    Email message changed

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -8,8 +8,8 @@ from .models import PasswordResetToken
 def send_email_with_token(sender, instance, **kwargs):
     target_email = instance.user.email
     send_mail(
-        'CalorieNotes: Password Reset Token',
-        f'Here is the password reset token: {instance.token}',
+        'CNotes: Password Reset Token',
+        f'Please paste it in your CNotes app. Here is the password reset token: {instance.token}',
         os.environ.get('EMAIL'),
         [target_email],
         fail_silently=False
